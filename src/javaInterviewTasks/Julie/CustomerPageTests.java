d5976c23f73763663ec115db18a358f36d897c37
Iuliia Zharkova <juliazharkova1993@gmail.com>
2025-12-31 13:17:00 -0500

    CustomerPage tests

---
+++ src/javaInterviewTasks/Julie/CustomerPageTests.java
@@ -0,0 +1,12 @@
+package javaInterviewTasks.Julie;
+
+public class CustomerPageTests {
+
+    public static void main(String[] args) {
+        // some tests here
+
+
+        //15 tests created
+
+    }
+}
